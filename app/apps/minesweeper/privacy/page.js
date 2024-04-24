56104f02126865e9938212c54c6d40dad58041e3
NyakoFox <nyakowofox@gmail.com>
2024-04-24 20:03:45 -0300

    add privacy page for minesweeper app

---
+++ app/apps/minesweeper/privacy/page.js
@@ -0,0 +1,28 @@
+
+export default function Privacy() {
+
+    return (
+        <>
+        <h1>PRIVACY POLICY</h1>
+
+        <p>We are committed to maintaining the accuracy, confidentiality, and security of your personally identifiable information ("Personal Information"). As part of this commitment, our privacy policy governs our actions as they relate to the collection, use and disclosure of Personal Information.</p>
+        <p>No identifiable information is collected from our users. No information which we collect is ever shared or sold. Our app uses third-party services which may store anonymous data.</p>
+
+        <h3>AdMob by Google</h3>
+        <p>AdMob uses advertising ID for a personalized experience with ads. <a href="https://policies.google.com/technologies/partner-sites">Click here to read about how Google uses your data.</a></p>
+
+        <h3>Opt-In</h3>
+        <p>By installing our app, you are consenting to your data being used by the third-party service above.</p>
+
+        <h3>Opt-Out</h3>
+        <p>You may opt-out of any data collection by uninstalling our app.</p>
+
+        <h3>Contact</h3>
+        <p>Any inquiries can be submitted to <strong>nyakowofox [at] gmail [dot] com</strong></p>
+
+        <hr/>
+
+        <footer>Last updated <strong>2023-03-21</strong>.</footer>
+    </>
+    )
+}
