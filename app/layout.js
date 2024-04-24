1fb75318a65200762132b1fc1782f04a6a3102a7
NyakoFox <nyakowofox@gmail.com>
2024-04-24 19:46:29 -0300

    add oembed data

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,28 @@ import "./globals.css";
 
 export const metadata = {
   title: "Nyako",
-  description: "hi!",
+  description: "my website! who am i?",
+  metadataBase: "https://nyako.gay/",
+  image: "/pfps/selene.png",
+  openGraph: {
+    title: "Nyako",
+    description: "my website! who am i?",
+    type: "website",
+    url: "https://nyako.gay/",
+    site_name: "Nyako",
+    locale: "en_US",
+    images: [
+        {
+            url: "/pfps/selene.png",
+            width: 512,
+            height: 512,
+            alt: "Nyako's pfp",
+        },
+    ],
+  },
+  twitter: {
+      card: "summary",
+  },
 };
 
 export default function RootLayout({ children }) {
