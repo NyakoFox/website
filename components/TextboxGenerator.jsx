6c90ea0cad18a78ba6e767ead0767ba61431e0a0
NyakoFox <nyakowofox@gmail.com>
2024-08-05 18:50:57 -0300

    color support

--- components/TextboxGenerator.jsx
+++ components/TextboxGenerator.jsx
@@ -3,6 +3,8 @@
 import { useEffect, useRef, useState } from "react";
 import styles from './TextboxGenerator.module.css';
 import { load_font, draw_text, get_text_size } from '@/src/font.js';
+import TextareaAutosize from 'react-textarea-autosize';
+import { RgbColorPicker, HexColorInput } from "react-colorful";
 
 export default function TextboxGenerator() {
     const [text, setText] = useState('');
@@ -10,7 +12,47 @@ export default function TextboxGenerator() {
     const [canvasWidth, setCanvasWidth] = useState(0);
     const [canvasHeight, setCanvasHeight] = useState(0);
     const [scale, setScale] = useState(2);
+
+    const [colorPickerOpen, setColorPickerOpen] = useState(false);
+
+    const [colors, setColors] = useState({
+        player: [164, 164, 255],
+        cyan: [164, 164, 255],
+        red: [255, 60, 60],
+        green: [144, 255, 144],
+        yellow: [255, 255, 134],
+        blue: [95, 95, 255],
+        purple: [255, 134, 255],
+        white: [244, 244, 244],
+        gray: [174, 174, 174],
+        orange: [255, 130, 20],
+        transparent: [0, 0, 0]
+    });
+
+    const [customColor, setCustomColor] = useState({r: 255, g: 255, b: 255});
+
+    const [color, setColor] = useState('player');
+
     const canvas = useRef(null);
+
+    function getForegroundColor(c = color) {
+        if (c === 'custom') {
+            if (customColor.r === 0 && customColor.g === 0 && customColor.b === 0) return 'rgb(255, 255, 255)';
+            return `rgb(${customColor.r}, ${customColor.g}, ${customColor.b})`;
+        }
+        if (c === 'transparent') return 'rgb(255, 255, 255)';
+        return `rgb(${colors[c].join(',')})`;
+    }
+
+    function getBackgroundColor(col = color) {
+        // divide r, g and b by 6, truncate
+        if (col === 'custom') {
+            if (customColor.r === 0 && customColor.g === 0 && customColor.b === 0) return 'rgb(0, 0, 0)';
+            return `rgb(${customColor.r / 6}, ${customColor.g / 6}, ${customColor.b / 6})`;
+        }
+        if (col === 'transparent') return 'rgb(0, 0, 0)';
+        return `rgb(${colors[col].map(c => Math.trunc(c / 6)).join(',')})`;
+    }
 
     useEffect(() => {
         load_font('font').then((font) => {
@@ -48,29 +90,61 @@ export default function TextboxGenerator() {
 
         ctx.imageSmoothingEnabled = false;
         ctx.scale(scale, scale);
-        // draw a box around the text
-        ctx.fillStyle = '#1B1B2A'
-        ctx.fillRect(0, 0, newWidth + 16, newHeight + 16);
-        ctx.fillStyle = '#A4A4FF'
-        ctx.fillRect(1, 1, newWidth + 14, newHeight + 14);
-        ctx.fillStyle = '#1B1B2A'
-        ctx.fillRect(3, 3, newWidth + 10, newHeight + 10);
-        ctx.fillStyle = '#A4A4FF'
-        ctx.fillRect(4, 4, newWidth + 8, newHeight + 8);
-        ctx.fillStyle = '#1B1B2A'
-        ctx.fillRect(5, 5, newWidth + 6, newHeight + 6);
+
+        if (color === 'transparent') {
+        }
+        else if (color === 'custom' && customColor.r === 0 && customColor.g === 0 && customColor.b === 0) {
+        }
+        else
+        {
+            // draw a box around the text
+            ctx.fillStyle = getBackgroundColor()
+            ctx.fillRect(0, 0, newWidth + 16, newHeight + 16);
+            ctx.fillStyle = getForegroundColor()
+            ctx.fillRect(1, 1, newWidth + 14, newHeight + 14);
+            ctx.fillStyle = getBackgroundColor()
+            ctx.fillRect(3, 3, newWidth + 10, newHeight + 10);
+            ctx.fillStyle = getForegroundColor()
+            ctx.fillRect(4, 4, newWidth + 8, newHeight + 8);
+            ctx.fillStyle = getBackgroundColor()
+            ctx.fillRect(5, 5, newWidth + 6, newHeight + 6);
+        }
 
         // draw the text
-        ctx.fillStyle = '#A4A4FF';
+        ctx.fillStyle = getForegroundColor();
 
         for (let i = 0; i < lines.length; i++) {
             draw_text(ctx, font, lines[i], 8, 8 + (height * i));
         }
 
-    }, [text]);
+    }, [text, color, customColor]);
 
     return <div className={styles.container}>
-        <textarea type="text" id="textbox" value={text} onChange={e => setText(e.target.value)}></textarea>
+        <button onClick={() => setColorPickerOpen(!colorPickerOpen)}>Pick color...</button>
+        {
+            colorPickerOpen && <div className={styles.colorPicker}>
+                {
+                    Object.keys(colors).map(c => <button key={c} style={{color: getForegroundColor(c), backgroundColor: getBackgroundColor(c), borderColor: getForegroundColor(c)}} onClick={() => setColor(c)}>{c}</button>)
+                }
+                <button onClick={() => setColor('custom')}>custom</button>
+            </div>
+        }
+
+        {
+            color === 'custom' && <>
+                <RgbColorPicker color={customColor} onChange={setCustomColor} />
+                <div className={styles.customColorInput}>
+                    <label htmlFor="customRed">R</label>
+                    <input id="customRed" type="number" value={customColor.r} onChange={e => setCustomColor({...customColor, r: e.target.value})} />
+                    <label htmlFor="customGreen">G</label>
+                    <input id="customGreen" type="number" value={customColor.g} onChange={e => setCustomColor({...customColor, g: e.target.value})} />
+                    <label htmlFor="customBlue">B</label>
+                    <input id="customBlue" type="number" value={customColor.b} onChange={e => setCustomColor({...customColor, b: e.target.value})} />
+                </div>
+            </>
+        }
+
+        <TextareaAutosize value={text} onChange={e => setText(e.target.value)} style={{resize: 'none'}} placeholder="Your text here..." />
 
         <canvas ref={canvas} style={{width: canvasWidth, height: canvasHeight}}></canvas>
 
