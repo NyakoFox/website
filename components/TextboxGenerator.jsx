3561f95c25cf9c8f228adfa8ec21be0a713e32dc
NyakoFox <nyakowofox@gmail.com>
2024-08-05 13:43:24 -0300

    rewrite textbox generator

--- components/TextboxGenerator.jsx
+++ components/TextboxGenerator.jsx
@@ -2,53 +2,78 @@
 
 import { useEffect, useRef, useState } from "react";
 import styles from './TextboxGenerator.module.css';
+import { load_font, draw_text, get_text_size } from '@/src/font.js';
 
 export default function TextboxGenerator() {
     const [text, setText] = useState('');
+    const [font, setFont] = useState(null);
+    const [canvasWidth, setCanvasWidth] = useState(0);
+    const [canvasHeight, setCanvasHeight] = useState(0);
+    const [scale, setScale] = useState(2);
     const canvas = useRef(null);
 
     useEffect(() => {
+        load_font('font').then((font) => {
+            console.log('Font loaded');
+            setFont(font);
+        });
+    }, [])
+
+    useEffect(() => {
+        if (!font) return;
+
         const ctx = canvas.current.getContext('2d');
+
+        // get text width and height
+        const { width, height } = get_text_size(font, text);
+
+        // calculate new width and new height, text might be multiline
+        const lines = text.split('\n');
+
+        // new width is just the width of the longest line
+        const newWidth = lines.reduce((acc, line) => {
+            const { width } = get_text_size(font, line);
+            return Math.max(acc, width);
+        }, 0);
+        const newHeight = height * lines.length;
+
+        canvas.current.width = (newWidth + 16) * scale;
+        canvas.current.height = (newHeight + 16) * scale;
+
+        setCanvasWidth(canvas.current.width);
+        setCanvasHeight(canvas.current.height);
 
         // clear canvas
         ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
 
-        // set up the text, anchored from the top-left
-        ctx.textRendering = 'geometricPrecision';
-        ctx.font = '8px Space Station';
-        ctx.textBaseline = 'top';
-        ctx.textAlign = 'left';
-
-        // get text width and height
-        const metrics = ctx.measureText(text);
-        const metricsHeight = ctx.measureText("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?\\`~");
-        const width = metrics.width;
-        const height = metricsHeight.actualBoundingBoxAscent + metricsHeight.actualBoundingBoxDescent;
-
+        ctx.imageSmoothingEnabled = false;
+        ctx.scale(scale, scale);
         // draw a box around the text
         ctx.fillStyle = '#1B1B2A'
-        ctx.fillRect(0, 0, width + 16, height + 16);
+        ctx.fillRect(0, 0, newWidth + 16, newHeight + 16);
         ctx.fillStyle = '#A4A4FF'
-        ctx.fillRect(1, 1, width + 14, height + 14);
+        ctx.fillRect(1, 1, newWidth + 14, newHeight + 14);
         ctx.fillStyle = '#1B1B2A'
-        ctx.fillRect(3, 3, width + 10, height + 10);
+        ctx.fillRect(3, 3, newWidth + 10, newHeight + 10);
         ctx.fillStyle = '#A4A4FF'
-        ctx.fillRect(4, 4, width + 8, height + 8);
+        ctx.fillRect(4, 4, newWidth + 8, newHeight + 8);
         ctx.fillStyle = '#1B1B2A'
-        ctx.fillRect(5, 5, width + 6, height + 6);
+        ctx.fillRect(5, 5, newWidth + 6, newHeight + 6);
 
         // draw the text
         ctx.fillStyle = '#A4A4FF';
-        ctx.fillText(text, 8, 8);
+
+        for (let i = 0; i < lines.length; i++) {
+            draw_text(ctx, font, lines[i], 8, 8 + (height * i));
+        }
+
     }, [text]);
 
     return <div className={styles.container}>
         <textarea type="text" id="textbox" value={text} onChange={e => setText(e.target.value)}></textarea>
 
-        <canvas ref={canvas} width="320" height="240"></canvas>
+        <canvas ref={canvas} style={{width: canvasWidth, height: canvasHeight}}></canvas>
 
         <p>This requires JS to be enabled.</p>
-        <p>If the text is blurry, try a different browser.</p>
-        <p>This is a work in progress, and might be completely rewritten in the future. Who knows.</p>
     </div>
 }
---
+++ src/font.js
@@ -0,0 +1,205 @@
+async function load_image(url)
+{
+    // create a promise, and resolve it when the image is loaded
+    return new Promise((resolve, reject) => {
+        const image = new Image()
+        image.src = url
+        image.onload = () => {
+            resolve(image)
+        }
+    })
+}
+
+function get_advance(font, char)
+{
+    if (char in font.special)
+    {
+        return font.special[char].advance
+    }
+    else
+    {
+        return font.width
+    }
+}
+
+export function get_text_size(font, text)
+{
+    const chars = font.chars
+    const width = font.width
+    const height = font.height
+
+    let cx = 0
+    let cy = 0
+
+    for (let i = 0; i < text.length; i++)
+    {
+        const char = text.codePointAt(i)
+        if (char >= 0x10000)
+        {
+            i++;
+        }
+
+        if (char in chars)
+        {
+            cx += get_advance(font, char)
+        }
+    }
+
+    return { width: cx, height: height }
+}
+
+export function draw_text(ctx, font, text, x, y)
+{
+    const chars = font.chars
+    const width = font.width
+    const height = font.height
+
+    let cx = x
+    let cy = y
+
+    // lets care about fillStyle... ugh
+
+    const newCanvas = document.createElement('canvas')
+    newCanvas.width = font.image.width
+    newCanvas.height = font.image.height
+    const newCtx = newCanvas.getContext('2d')
+    newCtx.drawImage(font.image, 0, 0)
+    const data = newCtx.getImageData(0, 0, font.image.width, font.image.height)
+
+    let r = 0
+    let g = 0
+    let b = 0
+    // Okay okay, let's parse ctx.fillStyle
+
+    if (ctx.fillStyle.startsWith('#'))
+    {
+        const hex = ctx.fillStyle.substring(1)
+        r = parseInt(hex.substring(0, 2), 16)
+        g = parseInt(hex.substring(2, 4), 16)
+        b = parseInt(hex.substring(4, 6), 16)
+    }
+    else if (ctx.fillStyle.startsWith('rgb'))
+    {
+        const rgb = ctx.fillStyle.substring(4, ctx.fillStyle.length - 1).split(',')
+        r = parseInt(rgb[0])
+        g = parseInt(rgb[1])
+        b = parseInt(rgb[2])
+    }
+    else
+    {
+        // it might just be a color name? is there an api for this? whatever, make it white
+        r = 255
+        g = 255
+        b = 255
+    }
+
+    for (let i = 0; i < data.data.length; i += 4)
+    {
+        data.data[i] = r
+        data.data[i + 1] = g
+        data.data[i + 2] = b
+    }
+    newCtx.putImageData(data, 0, 0)
+
+    for (let i = 0; i < text.length; i++)
+    {
+        const char = text.codePointAt(i)
+        if (char >= 0x10000)
+        {
+            i++;
+        }
+
+        if (char in chars)
+        {
+            const c = chars[char]
+            ctx.drawImage(newCanvas, c.x, c.y, width, height, cx, cy, width, height)
+            cx += get_advance(font, char)
+        }
+    }
+}
+
+export async function load_font(font_name)
+{
+    // Load the image
+    const image = await load_image(`/vvvvvv/fonts/${font_name}.png`)
+
+    // Load the XML
+    const parser = new DOMParser()
+    const request = await fetch(`/vvvvvv/fonts/${font_name}.fontmeta`)
+    const response = await request.text()
+    const xml = parser.parseFromString(response, 'text/xml')
+    const root = xml.documentElement
+
+    const font = {
+        type: root.getElementsByTagName('type')[0]?.textContent, // either unspecified (undefined here), or "buttons"
+        name: root.getElementsByTagName('display_name')[0]?.textContent ?? "Unnamed Font",
+        width: parseInt(root.getElementsByTagName('width')[0]?.textContent ?? 8),
+        height: parseInt(root.getElementsByTagName('height')[0]?.textContent ?? 8),
+        white_teeth: parseInt(root.getElementsByTagName('white_teeth')[0]?.textContent ?? 0),
+        fallback: root.getElementsByTagName('fallback')[0]?.textContent, // can be undefined!
+        chars: {},
+        special: {},
+        image: image
+    }
+
+    if (font.white_teeth == 0)
+    {
+        // Font isn't completely white, so let's make it white. every pixel should have its r, g and b values always 255. alpha is untouched.
+        const canvas = document.createElement('canvas')
+        canvas.width = image.width
+        canvas.height = image.height
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(image, 0, 0)
+        const data = ctx.getImageData(0, 0, image.width, image.height)
+        for (let i = 0; i < data.data.length; i += 4)
+        {
+            data.data[i] = 255
+            data.data[i + 1] = 255
+            data.data[i + 2] = 255
+        }
+        ctx.putImageData(data, 0, 0)
+        font.image = canvas
+    }
+
+    let x = 0
+    let y = 0
+
+    // loop through root -> chars
+    for (const range of root.getElementsByTagName('chars')[0].getElementsByTagName('range'))
+    {
+        const start = parseInt(range.getAttribute('start'))
+        const end = parseInt(range.getAttribute('end'))
+
+        for (let i = start; i <= end; i++)
+        {
+            font.chars[i] = {
+                x: x,
+                y: y
+            }
+
+            x += font.width
+            if (x >= image.width)
+            {
+                x = 0
+                y += font.height
+            }
+        }
+    }
+
+    // loop through root -> special
+    for (const special of root.getElementsByTagName('special')[0].getElementsByTagName('range'))
+    {
+        const start = parseInt(special.getAttribute('start'))
+        const end = parseInt(special.getAttribute('end'))
+
+        for (let i = start; i <= end; i++)
+        {
+            font.special[i] = {
+                advance: parseInt(special.getAttribute('advance')) ?? font.width,
+                color: parseInt(special.getAttribute('color') ?? 0)
+            }
+        }
+    }
+
+    return font
+}
