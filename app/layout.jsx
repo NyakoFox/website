95a0d267b4001fc51e3045b183dbb2aff68dfe2f
NyakoFox <nyakowofox@gmail.com>
2024-10-27 15:56:51 -0300

    taskbar (wip)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Taskbar from "@/components/Taskbar";
 
 export const metadata = {
   title: "Nyako",
@@ -29,7 +30,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Taskbar/>
+      </body>
     </html>
   );
 }
---
+++ components/Taskbar.jsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './Taskbar.module.css';
+
+export default function Taskbar()
+{
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [overlayOpen, setOverlayOpen] = useState(false);
+    const [shutOff, setShutOff] = useState(false);
+
+    return (
+        <div className={styles.taskbar}>
+            {
+                shutOff && <div className={styles.shutdown_anim}></div>
+            }
+            { overlayOpen && <div className={styles.overlay}>
+                <div className={styles.shutdown}>
+                    <div className={styles.shutdown_ok}     onClick={() => setShutOff(true)}></div>
+                    <div className={styles.shutdown_cancel} onClick={() => setOverlayOpen(false)}></div>
+                    <div className={styles.shutdown_close}  onClick={() => setOverlayOpen(false)}></div>
+                    <div className={styles.shutdown_help}   onClick={() => setOverlayOpen(false)}></div>
+                </div>
+            </div> }
+            <div className={`${styles.start} ${menuOpen && styles.start_clicked}`} onClick={() => setMenuOpen(!menuOpen)}>
+                {menuOpen &&
+                    <div className={styles.start_menu}>
+                        <div className={styles.start_menu_content}>
+                            <div className={styles.start_banner}></div>
+                            <div className={styles.start_menu_items}>
+                                <div className={`${styles.start_menu_button}`}>Test1</div>
+                                <div className={`${styles.start_menu_button}`}>Test2</div>
+                                <div className={`${styles.start_menu_button}`}>Test3</div>
+                                <div className={`${styles.start_menu_button}`}>Test4</div>
+                                <div className={`${styles.start_menu_button}`}>Test5</div>
+                                <div className={`${styles.start_menu_button} ${styles.start_menu_button_log_off}`}></div>
+                                <div className={`${styles.start_menu_button} ${styles.start_menu_button_shutdown}`}  onClick={() => setOverlayOpen(!overlayOpen)}></div>
+                            </div>
+                        </div>
+                    </div>
+                }
+            </div>
+            <div className={styles.icons}></div>
+            <div className={styles.windows}></div>
+            <div className={styles.tray}></div>
+        </div>
+    );
+}
